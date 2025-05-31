71d578edb6ebd91537b27fa5b19b38213a28530d
t170d025@gunma-u.ac.jp <t170d025@gunma-u.ac.jp>
2025-05-31 17:33:11 +0900

    a

--- PyAPI.cs
+++ PyAPI.cs
@@ -514,22 +514,3 @@ public static class ProcessExtentions
         return exited.Task;
     }
 }
-
-
-
-public class HashGenerator
-{
-    public static string Generate()
-    {
-        // ƒ‰ƒ“ƒ_ƒ€‚È•¶š—ñ‚ğ¶¬
-        string input = Guid.NewGuid().ToString();
-
-        using (SHA256 sha256 = SHA256.Create())
-        {
-            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
-            byte[] hashBytes = sha256.ComputeHash(inputBytes);
-            string hashString = BitConverter.ToString(hashBytes).Replace("-", "");
-            return hashString;
-        }
-    }
-}
