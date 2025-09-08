da4622547ff7271e142c5206d927ac2b8ab88deb
ZEPHYRUS\osuim <osuimino@gmail.com>
2025-09-08 23:51:23 +0900

    a

--- EnvSetter.cs
+++ EnvSetter.cs
@@ -1,13 +1,22 @@
 using MyUtil;
 using UnityEngine;
+using System;
 
 
 public class EnvSetter : MonoBehaviour
 {
     async void Start()
     {
-        //任意のフォルダパスを指定
-        await PyEnvSetter.ExeFlow($"{Application.streamingAssetsPath}/PythonAssets/Env", "3.12.5");
+        try
+        {
+            //任意のフォルダパスを指定
+            await PyEnvSetter.ExeFlow($"{Application.streamingAssetsPath}/PythonAssets/Env", "3.12.5");
+        }
+        catch(Exception e)
+        {
+            Debug.Log($"3 {e}");
+            throw;
+        }
         await VEnvSetter.ExeFlow($"{Application.streamingAssetsPath}/PythonAssets/Env");
     }
 }
