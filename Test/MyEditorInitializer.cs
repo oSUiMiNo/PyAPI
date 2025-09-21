815af28173204aa37bf2d0045059adaf833a3f59
ZEPHYRUS\osuim <osuimino@gmail.com>
2025-09-21 23:24:19 +0900

    no message

---
+++ Test/MyEditorInitializer.cs
@@ -0,0 +1,28 @@
+using UnityEditor;
+using UnityEngine;
+
+
+[InitializeOnLoad]
+public class MyEditorInitializer
+{
+    static bool initialized;
+
+    static MyEditorInitializer()
+    {
+        //if (!EditorPrefs.GetBool("MyEditorInitializer_Initialized", false))
+        //{
+        //    Debug.Log("MyEditorInitializerが初期化されました。");
+
+        //    EditorApplication.delayCall += OnEditorStartup;
+
+        //    EditorPrefs.SetBool("MyEditorInitializer_Initialized", true);
+        //}
+    }
+
+    static void OnEditorStartup()
+    {
+        Debug.Log("Editor起動後の処理を実行");
+        string savedValue = EditorPrefs.GetString("MySavedValue", "デフォルト値");
+        Debug.Log("保存された値：" + savedValue);
+    }
+}
