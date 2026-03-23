d5e8345763f3f4a2c0bfc4edc507f5606aff13cc
MaoOohira <oohiramaowork@gmail.com>
2026-03-24 03:52:20 +0900

    a

--- Tset_EnvSetting.cs
+++ Tset_EnvSetting.cs
@@ -5,7 +5,6 @@ public class Tset_EnvSetting : MonoBehaviour
 {
     async void Start()
     {
-        await PyEnvSetter.Exe($"{Application.streamingAssetsPath}/PythonAssets/Env", "3.12.5");
-        await VEnvSetter.Exe($"{Application.streamingAssetsPath}/PythonAssets/Env");
+        await UvSetter.Exe($"{Application.streamingAssetsPath}/PythonAssets/Env", "3.12.5");
     }
 }
